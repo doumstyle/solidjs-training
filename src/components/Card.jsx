a166cc376591d43b9eab2948528a5e507d7d45da
Mamadou N'Gatté <doumstyle@gmail.com>
2023-04-04 16:33:43 +0200

    add site banner and Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card() {
+  return (
+    <div>
+      <h2>Card Component</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti velit quibusdam quas, nihil eos similique?</p>
+    </div>
+  )
+}
